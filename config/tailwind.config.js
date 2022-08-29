57439b347a08865c1886fedfad7b8a879ee08f7d
Salman Ali <salmanpucit35@gmail.com>
2022-08-29 04:56:19 -0700

    configure groups add devise signup and signin pages wip

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,10 +2,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*'
   ],
   theme: {
     extend: {
@@ -20,3 +19,4 @@ module.exports = {
     require('@tailwindcss/typography'),
   ]
 }
+ 
